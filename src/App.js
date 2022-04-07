aae9fac584881db8314861306268d7e39ef807bc
mannguyen <nguyentrongminhman95@gmail.com>
2022-04-07 21:26:38 +0700

    upodate

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import { useEffect, useState } from "react";
-import axiosConfig from "./axiosConfig/axios";
+import CreateUser from "./component/CreateUser";
 function App() {
-  const SHEET_ID = "11O7lAcSXbwim6Z1mPqLGhyAK-_3FvxDpCUi1YLGPM8E";
-
-  const [data, setData] = useState([]);
-  const getSheetValues = async () => {
-    try {
-      const response = await axiosConfig.get(
-        `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/A:F`
-      );
-      setData(response.values);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  useEffect(() => {
-    getSheetValues();
-  }, []);
-
   return (
     <div className="App">
-      {data ? (
-        <div className="data">
-          {data.map((item, i) => (
-            <span key={i}>{item}</span>
-          ))}
-        </div>
-      ) : null}
+      <CreateUser />
     </div>
   );
 }
---
+++ src/component/CreateUser.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+const CreateUser = () => {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+  return (
+    <div className="create-user">
+      <Form
+        name="basic"
+        labelCol={{
+          span: 4,
+        }}
+        wrapperCol={{
+          span: 20,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 4,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default CreateUser;
