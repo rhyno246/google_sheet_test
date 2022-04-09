3060ea7e3d8778795a8638beef9b06efed41e076
mannguyen <nguyentrongminhman95@gmail.com>
2022-04-09 12:09:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import CreateUser from "./component/CreateUser";
+import { useEffect } from "react";
+import axiosConfig from "./axiosConfig/axios";
+
 function App() {
-  return (
-    <div className="App">
-      <CreateUser />
-    </div>
-  );
+  const getData = async () => {
+    try {
+      const data = await axiosConfig.get(
+        `https://script.google.com/macros/s/AKfycby3U4LPKDJHTSV9yRk5wiEH-6q2xy3CsoWLCxg28jJfTFzYGQo/exec`
+      );
+      console.log(data);
+    } catch (error) {}
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return <div className="App">dssdsd</div>;
 }
 
 export default App;
--- src/axiosConfig/axios.js
+++ src/axiosConfig/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 const axiosConfig = axios.create({
   headers: {
+    baseURL:
+      "https://script.google.com/macros/s/AKfycby3U4LPKDJHTSV9yRk5wiEH-6q2xy3CsoWLCxg28jJfTFzYGQo/exec",
+    "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   },
 });
